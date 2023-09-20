a087937e8ba0e7df9ab8522696cc3acaf48ab740
Gauravk9870 <78679960+Gauravk9870@users.noreply.github.com>
2023-09-20 19:43:35 +0530

    add mobile nav menu

--- src/app/components/navbar/Navbar.js
+++ src/app/components/navbar/Navbar.js
@@ -12,58 +12,89 @@ import Link from 'next/link';
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.container}>
-                <div className={styles.logo} >
-                    <Link href="/">
-                        <span>
-                            Gaurav
-                            <span className={styles.dot}>
-                                .
-                            </span>
-                        </span>
-                    </Link>
-                </div>
-                <ul className={styles.links}>
-                    <li>
+        <>
 
+            <nav className={styles.navbar}>
+                <div className={styles.container}>
+                    <div className={styles.logo} >
                         <Link href="/">
-                            Home
-                        </Link> </li>
-                    <li>About</li>
-                    <li>
-                        <Link href="/projects">
-                            Projects
+                            <span>
+                                Gaurav
+                                <span className={styles.dot}>
+                                    .
+                                </span>
+                            </span>
                         </Link>
-                    </li>
-                    <li>Contact</li>
-                </ul>
-                <ul className={styles.social}>
-                    <li>
-                        <a href="https://github.com/Gauravk9870" target='_blank'>
-                            <AiFillGithub />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/gauravk9870/" target='_blank'>
-                            <FaLinkedinIn />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.instagram.com/gaurav.k__/" target='_blank'>
-                            <AiOutlineInstagram />
-                        </a>
-                    </li>
-                </ul>
-                <div className={`${styles['menu-btn']} ${isOpen ? styles.animate : ""}`} onClick={() => setIsOpen(!isOpen)}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                    </div>
+                    <ul className={styles.links}>
+                        <li>
+                            <Link href="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/#experience">
+                                About
+                            </Link></li>
+                        <li>
+                            <Link href="/projects">
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/gauravk9870/" target='_blank'>Contact</a>
+                        </li>
+                    </ul>
+                    <ul className={styles.social}>
+                        <li>
+                            <a href="https://github.com/Gauravk9870" target='_blank'>
+                                <AiFillGithub />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/gauravk9870/" target='_blank'>
+                                <FaLinkedinIn />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/gaurav.k__/" target='_blank'>
+                                <AiOutlineInstagram />
+                            </a>
+                        </li>
+                    </ul>
+                    <div className={`${styles['menu-btn']} ${isOpen ? styles.animate : ""}`} onClick={() => setIsOpen(!isOpen)}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
-            </div>
-        </nav >
+            </nav >
+            {
+                isOpen &&
+                <div className={styles.mobile}>
+                    <ul className={styles.links}>
+                        <li>
+                            <Link href="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/#experience">
+                                About
+                            </Link></li>
+                        <li>
+                            <Link href="/projects">
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/gauravk9870/" target='_blank'>Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            }
+        </>
     );
 };
 
